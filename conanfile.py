ed77ce012625c159754ef73ffdc24d362a73f907
thai.phi <thai.phi@indigames.net>
2021-06-04 16:38:52 +0700

    Simplify script build.

--- conanfile.py
+++ conanfile.py
@@ -13,57 +13,13 @@ class IgeConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
-    exports_sources = ""
-    requires = []
+    exports_sources = "*.h*"
+    no_copy_source = True
     short_paths = True
 
-    def requirements(self):
-        if (self.settings.os == "Windows") and (self.name != "zlib"):
-            self.requires("zlib/1.2.11@ige/test")
-
     def package(self):
-        output_dir = os.path.normpath(os.path.relpath(os.environ['OUTPUT_DIR'], os.environ['PROJECT_DIR']))
-        self.copy("*", dst="include", src=os.path.join(output_dir, "include"))
-        if self.settings.os == "Windows":
-            if self.settings.arch == "x86":
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/windows/x86"))
-            else:
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/windows/x86_64"))
-        elif self.settings.os == "Android":
-            if self.settings.arch == "armv7":
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/android/armv7"))
-            elif self.settings.arch == "armv8":
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/android/armv8"))
-            elif self.settings.arch == "x86":
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/android/x86"))
-            elif self.settings.arch == "x86_64":
-                self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/android/x86_64"))
-        elif self.settings.os == "Macos":
-            self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/macos/x64"))
-        else:
-            self.copy("*", dst="lib", src=os.path.join(output_dir, "libs/ios/arm64"))
+        self.copy("*.h*", dst="include/taskflow", src="taskflow")
+        self.run(f'conan upload {self.name}/{self.version}@ige/test --remote ige-center --force --confirm')
 
-    def package_info(self):
-        self.cpp_info.libs = self.__collect_lib('lib')
-        self.cpp_info.includedirs  = self.__collect_include('include')
-        self.cpp_info.defines = [f'USE_{self.name}'.upper()]
-
-    def __collect_lib(self, lib_dir):
-        libs = []
-        for root, dirs, files in os.walk(lib_dir):
-            for file in files:
-                if file.endswith(".lib"):
-                    fname = os.path.splitext(file)[0]
-                    libs.append(fname)
-                elif file.endswith(".a"):
-                    fname = os.path.splitext(file)[0]
-                    if fname.startswith('lib'):
-                        fname = fname[fname.find('lib') + 3:]
-                    libs.append(fname)
-        if (self.settings.os == "Windows") and (self.name != "zlib"):
-            libs.append('zlibstatic')
-        return libs
-
-    def __collect_include(self, inc_dir):
-        inc_dirs = ['include']
-        return inc_dirs
+    def package_id(self):
+        self.info.header_only()
